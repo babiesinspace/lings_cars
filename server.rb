bc4b3ee255ec39642b8832ff99a0ce0e729ca041
babiesinspace <allie.g.cooper@gmail.com>
2018-04-25 14:09:22 -0400

    add admin class. configure and test sessions

--- server.rb
+++ server.rb
@@ -1,7 +1,22 @@
 require "sinatra"
 require "sinatra/activerecord"
+require "shotgun"
 
 require_relative './models/car'
 require_relative './models/user'
 
 set :database, {adapter: 'postgresql', database: 'lings-cars'}
+
+configure do
+    enable :sessions unless test?
+    set :session_secret, "secret"
+ end
+
+get '/' do 
+	session[:message] = "Hello world"
+	"#{session[:message]}"
+end 
+
+get '/message' do 
+	"#{session[:message]}"
+end 
